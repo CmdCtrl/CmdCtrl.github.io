ac0b807b7962df7883b36be633d885fb031b525f
CmdCtrl <thecmdctrl@gmail.com>
2020-08-30 19:54:19 -0400

    markup basics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-title">
+      <img src="https://themindsetproject.com.au/wp-content/uploads/2017/08/avatar-icon.png" alt="" class="avi">
+      <div class="title-text-content">
+        <p>About Me<p>
+        <h2>Jordan Lyon</h2>
+      </div>
+      <div class="content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Ex enim suscipit aliquam recusandae illum autem magni commodi explicabo, 
+          culpa corporis perspiciatis quod. 
+          Quod vitae dolorum et blanditiis, dolore ipsam cum?</p>
+          <table>
+            <tr>
+              <th>Skills</th>
+              <th>Technologies</th>
+            </tr>
+            <tr>
+              <td>Design</td>
+              <td>Photoshop, Blender, Unity</td>
+            </tr>
+            <tr>
+              <td>Front-End</td>
+              <td>Vue.js, HTML, CSS, JavaScript</td>
+            </tr>
+            <tr>
+              <td>Back-End</td>
+              <td>Node.js, MongoDB, mySQL, MariaDB</td>
+            </tr>
+            <tr>
+              <td>System Programming</td>
+              <td>C</td>
+            </tr>
+            <tr>
+              <td>Object Oriented</td>
+              <td>Java, Python</td>
+            </tr>
+            <tr>
+              <td>Environments</td>
+              <td>Eclipse, Vim, Linux</td>
+            </tr>
+            <tr>
+              <td>Other</td>
+              <td>Microsoft Office (Word, PPT, Excel), slack, TypeScript, git</td>
+            </tr>
+          </table>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Nostrum enim deserunt sapiente quae quasi sequi quam, explicabo impedit eligendi sint incidunt, 
+            mollitia reprehenderit exercitationem cumque nobis.
+             Eius necessitatibus provident nostrum.</p>
+      </div>
+    </div>
   </div>
 </template>
